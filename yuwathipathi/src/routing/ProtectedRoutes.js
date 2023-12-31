48c7a82eeae5ccc3fd313cec5d992a8860e01e07
Rajitha Rathnayaka <rajitha.rathnayaka@acentura.com>
2024-01-01 00:25:48 +0530

    unAuthorized page updated

--- yuwathipathi/src/routing/ProtectedRoutes.js
+++ yuwathipathi/src/routing/ProtectedRoutes.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { NavLink, Outlet} from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import Login from '../pages/Login'
+import style from './ProtectedRoutes.module.css'
+import Unauthorize from '../assets/Unauthorize.jpg'
+
 
 const ProtectedRoute = () => {
   const { userInfo } = useSelector((state) => state.auth)
@@ -9,11 +12,14 @@ const ProtectedRoute = () => {
   // show unauthorized screen if no user is found in redux store
   if (!userInfo) {
     return (
-      <div className='unauthorized'>
-        <h1>Unauthorized :</h1>
-        <span>
-          <NavLink to='/login'>Login</NavLink> to gain access
-        </span>
+      <div className={style.unauthorized} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#e6e9ff', borderRadius: '0.4rem' }}>
+        <div style={{ textAlign: 'center' }}>
+          <h1>Unauthorized</h1>
+          <p>
+            <NavLink to='/login'>Login</NavLink> to gain access
+          </p>
+          <img src={Unauthorize} alt="Unauthorized" width="500" height="" style={{ paddingTop:'2rem'}}/>
+        </div>
       </div>
     )
   }
